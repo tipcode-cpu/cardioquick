03cb72ea4cb84279112c09e6d083c505b4a916fa
tipcode-cpu <tipcode@gmail.com>
2026-06-22 14:48:38 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'cardioquick-pwa-v14-ldl-copy';
+const CACHE_NAME = 'cardioquick-pwa-v13-select-overflow';
 const ASSETS = ['./', './index.html', './manifest.json', './service-worker.js', './icon.svg', './zip-sdi.txt'];
+
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
@@ -11,6 +13,7 @@ self.addEventListener('activate', event => {
       .then(() => self.clients.claim())
   );
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request))
